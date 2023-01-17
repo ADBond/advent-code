28fcfd271760f262270ebbf7925734cc9189db62
ADBond <48208438+ADBond@users.noreply.github.com>
2023-01-18 08:38:54 +1100

    valves day 16 stub

---
+++ 2022/16_valves.jl
@@ -0,0 +1,12 @@
+module constants
+export inputfilename, inputfilenametest
+inputfilename = "2022/16valves.input.txt"
+inputfilenametest = "2022/16valves_test.input.txt"
+end
+
+module v1
+using ..constants
+
+println("Part 1:")
+
+end
